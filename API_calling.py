8727705e49936d23a535d79eefc17e8c05922b9a
namanjain123 <naman.nishi@gmail.com>
2023-12-15 00:27:47 +0530

    Updated the api

--- API_calling.py
+++ API_calling.py
@@ -1,14 +1,17 @@
-from flask import Flask, request
+from flask import Flask, jsonify, request
+import LLM_Get_DishesName as ChefLLM;
+
+# create a Flask 
 app = Flask(__name__)
 
-
-@app.route('/api2', methods=['POST'])  
-def api2():
-   data = request.get_json()
-   # process data
-   return "OK"
-@app.route('/api1', methods=['POST'])
+@app.route('/api/dishes', methods=['POST'])
 def api1():
-   data = request.get_json()
-   # process data
-   return "OK"
+   try:
+      data = request.get_json()
+      ingrident = data['ingrident']
+      details=data['details']
+      pointers=data['pointers']
+      list_dishes=ChefLLM.get_Dishes_option(ingrident,details,pointers)
+      return jsonify(list_dishes)
+   except Exception as e:
+        return jsonify({"error": str(e)}), 400
